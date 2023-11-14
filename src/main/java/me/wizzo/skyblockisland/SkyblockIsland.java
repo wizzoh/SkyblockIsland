4b3348e6ed2dd9bfc55a133003b847c41e582322
wizzoh <lollocoppa04@gmail.com>
2023-11-14 18:18:30 +0100

    Aggiunto creazione cartella e file config + chatcolor translate

--- src/main/java/me/wizzo/skyblockisland/SkyblockIsland.java
+++ src/main/java/me/wizzo/skyblockisland/SkyblockIsland.java
@@ -1,17 +1,50 @@
 package me.wizzo.skyblockisland;
 
+import me.wizzo.skyblockisland.files.ConfigFile;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SkyblockIsland extends JavaPlugin {
 
+    private final ConsoleCommandSender console = Bukkit.getConsoleSender();
+    private String prefix;
+    private ConfigFile configFile;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        createFolder();
+        //Island command
+        files();
+        //Listeners
+        this.prefix = configFile.get().getString("Prefix");
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+    }
+
+    private void createFolder() {
+        if (!getDataFolder().exists()) {
+            boolean success = getDataFolder().mkdirs();
+            if (!success) {
+                console.sendMessage(colorsMessage(prefix + "&cImpossibile creare la cartella, disabilitazione plugin in corso.."));
+                getPluginLoader().disablePlugin(this);
+            }
+        }
+    }
+
+    private void files() {
+        this.configFile = new ConfigFile(this);
+    }
+
+    public String colorsMessage(String message) {
+        return ChatColor.translateAlternateColorCodes('&', message);
+    }
+
+    public ConsoleCommandSender getConsole() {
+        return console;
     }
 }
