7dfac7b7fba28a6c3e91e050eb928bae16293b6e
wizzoh <lollocoppa04@gmail.com>
2023-11-14 17:21:09 +0100

    Primo commit, file base generati

---
+++ src/main/java/me/wizzo/skyblockisland/SkyblockIsland.java
@@ -0,0 +1,17 @@
+package me.wizzo.skyblockisland;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SkyblockIsland extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
