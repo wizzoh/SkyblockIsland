7918cf74443f24eba73a860ff893069aaf4e2ec4
wizzoh <lollocoppa04@gmail.com>
2023-11-15 15:33:39 +0100

    Gestore per copiare, cancellare, caricare, scaricare e resettare le isole

---
+++ src/main/java/me/wizzo/skyblockisland/manager/IslandManager.java
@@ -0,0 +1,82 @@
+package me.wizzo.skyblockisland.manager;
+
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.WorldCreator;
+import org.bukkit.util.FileUtil;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+
+public class IslandManager {
+
+    public static void cloneWorld(String sourceFolderName, String destinationFolderName) {
+        File source = null;
+        
+        try {
+            source = new File(sourceFolderName);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        if (source != null) {
+           File destination = new File(destinationFolderName);
+
+           try {
+               copyFolder(source.toPath(), destination.toPath());
+           } catch (IOException e) {
+               e.printStackTrace();
+           }
+        }
+        loadWorld(destinationFolderName);
+    }
+
+    public static void deleteWorld(File worldFolder) {
+        unloadWorld(worldFolder.getName());
+        if (worldFolder.exists()) {
+            File[] files = worldFolder.listFiles();
+            if (files != null){
+                for (File file: files) {
+                    if (file.isDirectory()) {
+                        deleteWorld(file);
+                    } else {
+                        file.delete();
+                    }
+                }
+            }
+            worldFolder.delete();
+        }
+    }
+
+    public static void resetWorld(File worldFile, String sourceFolderName, String destinationFolderName) {
+        deleteWorld(worldFile);
+        cloneWorld(sourceFolderName, destinationFolderName);
+    }
+
+    public static void unloadWorld(String worldName) {
+        Bukkit.getServer().unloadWorld(worldName, true);
+    }
+
+    public static void loadWorld(String worldName) {
+        Bukkit.getServer().createWorld(new WorldCreator(worldName));
+    }
+
+    private static void copyFolder(Path source, Path destination) throws IOException {
+        Files.walk(source).forEach(sourcePath -> {
+            try {
+                Path destinationPath = destination.resolve(source.relativize(sourcePath));
+
+                if (Files.isDirectory(sourcePath)) {
+                    Files.createDirectories(destinationPath);
+                } else {
+                    Files.copy(sourcePath, destinationPath, StandardCopyOption.REPLACE_EXISTING);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+}
